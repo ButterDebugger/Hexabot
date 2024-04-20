9decf069e090ef3681faf062c6bb75f4228e7f13
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-04-20 00:29:46 -0400

    Fixed spacing

--- bot.js
+++ bot.js
@@ -10,53 +10,55 @@ import initAutoStickyRoles from "./autoStickyRoles.js";
 const { custom_activity } = ConfigManager.botConfig;
 
 const bot = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildBans, GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildInvites,
-    ]
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildModeration,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildInvites
+	]
 });
 
 bot.ConfigManager = ConfigManager;
 bot.DataManager = DataManager;
 
 bot.on("ready", async () => {
-    // Start required modules
-    await ConfigManager.refresh(bot);
-    await ConfigManager.init(bot);
+	// Start required modules
+	await ConfigManager.refresh(bot);
+	await ConfigManager.init(bot);
 
-    initCmd(bot);
+	initCmd(bot);
 
-    // Start bot modules
-    initLogger(bot);
-    initCounting(bot);
-    initJoinLeaveMsgs(bot);
-    initAutoStickyRoles(bot);
+	// Start bot modules
+	initLogger(bot);
+	initCounting(bot);
+	initJoinLeaveMsgs(bot);
+	initAutoStickyRoles(bot);
 
-    // Set bot status and activity
-    console.log(custom_activity)
-    bot.user.setStatus(custom_activity.status);
+	// Set bot status and activity
+	bot.user.setStatus(custom_activity.status);
 
-    const updateActivity = () => {
-        let actIndex = Math.floor(Math.random() * custom_activity.activities.length);
-        let activity = custom_activity.activities[actIndex];
+	const updateActivity = () => {
+		let actIndex = Math.floor(
+			Math.random() * custom_activity.activities.length
+		);
+		let activity = custom_activity.activities[actIndex];
 
-        if (typeof activity == "object" && typeof activity?.type == "string") {
-            activity.type = ActivityType[activity.type];
-        }
+		if (typeof activity == "object" && typeof activity?.type == "string") {
+			activity.type = ActivityType[activity.type];
+		}
 
-        bot.user.setActivity(activity);
-    };
+		bot.user.setActivity(activity);
+	};
 
-    updateActivity();
-    setInterval(updateActivity, custom_activity.interval);
+	updateActivity();
+	setInterval(updateActivity, custom_activity.interval);
 
-    // Log bot startup
-    console.log(`Logged in as ${bot.user.tag}!`);
+	// Log bot startup
+	console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.login(process.env.BOT_TOKEN);
